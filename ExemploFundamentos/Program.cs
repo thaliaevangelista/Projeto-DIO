4910e17d80d6a2ffab739943515aaa5f59bf10ea
Thalia <thalia.soares.e27@gmail.com>
2026-05-31 19:01:38 -0300

    insrindo int e string

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -1,7 +1,32 @@
 ﻿using ExemploFundamentos.Models;
 
-Pessoa pessoa1 = new Pessoa();
 
+string a = "15-";
+int b = 0;
+
+int.TryParse(a, out b);
+
+Console.WriteLine(b);
+Console.WriteLine("Conversão realizada com sucesso!");
+
+
+
+
+//int a = 5;
+//double b = a;
+
+//int a = 5;
+//long b = a;
+
+
+//double a = 4 / 2 +2;
+
+//Console.WriteLine(a);
+
+
+
+
+/*Pessoa pessoa1 = new Pessoa();
 pessoa1.Nome = "Thalia";
 pessoa1.Idade = 28;
-pessoa1.Apresentar();
+pessoa1.Apresentar();*/
