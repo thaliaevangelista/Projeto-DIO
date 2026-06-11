fecdcc5f9125fcc8b455ac6e66786fa657f8e79f
Thalia Evangelista <thalia.soares.e27@gmail.com>
2026-06-11 00:14:57 -0300

    fix: removendo submódulos e adicionando pastas como normais

---
+++ ExemploExplorando/Program.cs
@@ -0,0 +1,42 @@
+﻿using ExemploExplorando.Models;
+
+Pessoa p1 = new Pessoa(nome:"Thalia", sobrenome:"Soares");
+Pessoa p2 = new Pessoa(nome:"Alex", sobrenome: "Rafael");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// p1.Idade = 28;
+
+// p1.Apresentar();
+
+
+
+
